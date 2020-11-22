44412156a0ac2b35c63da24dd3cdbcc9d42f6589
Mika Berglund <mika.berglund@meteoriitti.com>
2020-11-22 12:54:08 +0200

    Added Functions app with initial HTTP endpoint.

---
+++ FunctionApp1/Names.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp1
+{
+    public static class Names
+    {
+
+        /// <summary>
+        /// The HTTP (PATCH) triggered function that triggers a new user processing orchestration instance.
+        /// </summary>
+        public const string TriggerUserProcessingHttp = nameof(TriggerUserProcessingHttp);
+
+    }
+}
---
+++ FunctionApp1/UserProcessingFunctions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionApp1
+{
+    public class UserProcessingFunctions
+    {
+
+        [FunctionName(Names.TriggerUserProcessingHttp)]
+        public async Task<HttpResponseMessage> TriggerUserProcessingHttp([HttpTrigger(AuthorizationLevel.Function, "PATCH", Route = "TriggerUserProcessing/{userId}")]HttpRequestMessage request, string userId)
+        {
+
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+    }
+}
